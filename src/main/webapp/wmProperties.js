9ae8ea7a40de1863b28149e062ad222a19e75eb2
root <root@329fd6f4d460>
2016-10-23 09:28:34 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "defaultLanguage" : "en",
+  "displayName" : "WaveSwipe",
+  "homePage" : "Main",
+  "name" : "WaveSwipe",
+  "platformType" : "MOBILE",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
